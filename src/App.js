eb30ad387600b9a6dbdcdefa63c3cb8e7f09ac6f
nici <niciputter@yahoo.com>
2019-04-02 18:58:19 +0200

    connect to kimai api, establish connection

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 class App extends Component {
-  async getKimai() {
+  async getPing() {
     let apiUrl = "";
     const proxyUrl = "https://cors-anywhere.herokuapp.com/";
     if (process.env.NODE_ENV === "development") {
@@ -19,9 +19,35 @@ class App extends Component {
       const response = await axios.get(apiUrl + "ping", {
         headers: headers
       });
-      const kimai = await response;
-      console.log(kimai);
-      return kimai;
+      const ping = await response;
+      console.log(ping);
+      return ping;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async getTimesheets() {
+    let apiUrl = "";
+    const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+    if (process.env.NODE_ENV === "development") {
+      apiUrl = proxyUrl + "http://demo.kimai.org/api/";
+      console.log(process.env.NODE_ENV);
+    } else {
+      apiUrl = "https://demo.kimai.org/api/";
+      console.log(process.env.NODE_ENV);
+    }
+    let headers = {
+      "X-AUTH-USER": "susan_super",
+      "X-AUTH-TOKEN": "api_kitten"
+    };
+    try {
+      const response = await axios.get(apiUrl + "timesheets", {
+        headers: headers
+      });
+      const times = await response;
+      console.log(times);
+      return times;
     } catch (error) {
       console.error(error);
     }
@@ -30,8 +56,9 @@ class App extends Component {
   render() {
     return (
       <>
-        <p>hello</p>
-        <button onClick={() => this.getKimai()}>ping</button>
+        <p>Click response you wanna get</p>
+        <button onClick={() => this.getPing()}>ping</button>
+        <button onClick={() => this.getTimesheets()}>timesheets</button>
       </>
     );
   }
